622f254f585f5177211db98566cfebddce74a8a7
joshsarna <joshsarna@gmail.com>
2018-10-25 14:48:37 -0700

    use task template in task-list template

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 /* global Vue, VueRouter, axios */
 
+Vue.component('task-list', {
+  template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+
+  data() {
+    return {
+      tasks: [
+        { description: 'Learn French', completed: false },
+        { description: 'Learn Spanish', completed: false },
+        { description: 'Learn German', completed: true }
+
+      ]
+    };
+  }
+
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 
