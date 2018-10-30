bacccc25823d7483e7cc254684981eb9577d5abe
joshsarna <joshsarna@gmail.com>
2018-10-30 11:27:32 -0700

    add dynamic bulma navbar using computed, mounted, and props elements of components

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* global Vue, VueRouter, axios */
 
+Vue.component('tabs', {
+  template: `
+    <div>
+      <div class="tabs">
+        <ul>
+          <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+            <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+
+  data() {
+    return { tabs: [] };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name === selectedTab.name);
+      });
+    }
+  }
+});
+
+Vue.component('tab', {
+  template: `
+    <div v-show="isActive"><slot></slot></div>
+  `,
+
+  props: {
+    name: { required: true },
+    selected: { default: false }
+  },
+
+  data() {
+    return { isActive: false };
+  },
+
+  computed: {
+    href() {
+      return '#' + this.name.toLowerCase().replace(/ /g, '-');
+    }
+  },
+
+  mounted() {
+    this.isActive = this.selected;
+  }
+});
+
 Vue.component('modal', {
   template: `
     <div class="modal is-active">
@@ -7,7 +66,7 @@ Vue.component('modal', {
       <div class="modal-content">
         <div class="box">
           <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. 
+            <slot></slot> 
           </p>
         </div>
       </div>
